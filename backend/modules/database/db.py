acbf3a8e64d01f66fce7d055b74cf2f38696fdd6
jc288 <jendycaprico@gmail.com>
2025-04-16 15:48:04 +0700

    feat(vector): add base vector implementation

--- backend/modules/database/db.py
+++ backend/modules/database/db.py
@@ -3,19 +3,26 @@ load_dotenv()
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from .models import Base, SalesReps, Deals, Clients, Skills
+from .llm_models import LLMBase
+from modules.llm.vector_db import insert_docs
 import json
 import os
 
 DB_PATH = os.environ.get("DB_PATH")
+LLM_DB_PATH = os.environ.get("LLM_DB_PATH")
 
 engine = create_engine(f"sqlite://{DB_PATH}", echo=True)
-
-
+llm_engine = create_engine(f"sqlite://{LLM_DB_PATH}", echo=True)
 
 def init_db():
   Base.metadata.create_all(engine)
   
   return engine
+
+def init_llm_db():
+  LLMBase.metadata.create_all(llm_engine)
+  
+  return llm_engine
       
 
 def migrate():
@@ -46,6 +53,9 @@ def migrate():
   
   with Session(engine) as session:
     all_reps = []    
+    docs = []
+    industries = {}
+    
     for sales_reps in DUMMY_DATA['salesReps']:
       # reps = SalesReps(**sales_reps)
       reps = SalesReps(
@@ -60,9 +70,51 @@ def migrate():
         
       all_reps.append(reps)
       
+      base_str = f"Representative: {reps.name}, Role: {reps.role}, Region: {reps.region}"
+      
+      skill_str = f"{base_str} has skills {", ".join(sales_reps['skills'])}"
+      
+      deals_str = f"{base_str} has deals with {", ".join(deal.client for deal in reps.deals)} with total value of {sum(deal.value for deal in reps.deals)}"
+      
+      deals_docs = [f"{base_str} has deal with {deal.client} for value of {deal.value} with status of {deal.status}" for deal in reps.deals]
+      
+      client_str = f"{base_str} current clients are {", ".join(client.name for client in reps.clients)}"
+      
+      for client in reps.clients:
+        if client.industry not in industries:
+          industries[client.industry] = []
+          
+        industries[client.industry].append(client)
+      
+      datas = [skill_str, deals_str, client_str, *deals_docs]
+      datas = [
+        {
+          "text": text, 
+          "reps_name": reps.name, 
+          "reps_region": reps.region,
+          "industry": None
+        } 
+        for text in datas
+      ]
+      docs = [*docs, *datas]
       
     session.add_all(all_reps)    
     session.commit()
-
-        
+    
+    for key, value in industries.items():
+      industry_str = f"{key} industry clients are {", ".join(client.name for client in value)}"
+      
+      data = {
+        "text": industry_str,
+        "industry": key,
+        "reps_name": None,
+        "reps_region": None,
+      }
+      
+      docs.append(data)
+    
+    res = insert_docs(docs)
+    print("=== Insert Vector Result ===")
+    print(res)
+    print("=== Insert Vector Result ===")
     
