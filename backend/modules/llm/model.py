9a4d7e480b3adb6e81656e2803d0f24ff6e69983
jc288 <jendycaprico@gmail.com>
2025-04-16 17:49:06 +0700

    feat(llm): implement response caching

--- backend/modules/llm/model.py
+++ backend/modules/llm/model.py
@@ -1,12 +1,20 @@
 from dotenv import load_dotenv
 load_dotenv()
 from langchain_groq import ChatGroq
+from langchain.globals import set_llm_cache, set_verbose
+from langchain.cache import SQLAlchemyCache
+from modules.database.db import init_llm_db
 import os
 
+class CustomSQLiteCache(SQLAlchemyCache):
+  def __init__(self, engine):
+    super().__init__(engine)
+
+set_llm_cache(SQLAlchemyCache(init_llm_db()))
+set_verbose(True if os.environ.get("LLM_VERBOSE") else False)
 
 def get_model(
   temp=0.4, 
-  verbose=True,
   max_tokens=5000,
   model_name="llama-3.3-70b-versatile",
   max_retries=2,
@@ -19,7 +27,6 @@ def get_model(
   return ChatGroq(
     api_key=API_KEY,
     temperature=temp,
-    verbose=verbose,
     max_retries=max_retries,
     max_tokens=max_tokens,
     model=model_name,
