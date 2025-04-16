d604b2355279c38d73fad912bf541f38dc53cfd8
jc288 <jendycaprico@gmail.com>
2025-04-16 17:16:13 +0700

    fix(backend): update deals missing status update feat(backend): continuing vector store implementation

--- backend/dto/ai_dto.py
+++ backend/dto/ai_dto.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel, Field
+from typing import TypedDict, Optional, Any
+from datetime import datetime
 
 
 class AIPromptDTO(BaseModel):
@@ -6,3 +8,31 @@ class AIPromptDTO(BaseModel):
   prompt: str = Field()
 
 
+class VectorDoc(TypedDict):
+  text: str
+  industry: Optional[str] = None
+  reps_name: Optional[str] = None
+  reps_region: Optional[str] = None
+  vector: Any
+  created_at: int 
+  
+  
+  @classmethod
+  def build(
+    cls,
+    text:str,
+    industry = None,
+    reps_name = None,
+    reps_region = None,
+    vector = None,
+    created_at = int(datetime.now().strftime("%s"))
+  ):
+    print(text)
+    return VectorDoc(
+      text=text,
+      industry=industry,
+      reps_name=reps_name,
+      reps_region=reps_region,
+      vector=vector,
+      created_at=created_at
+    )
