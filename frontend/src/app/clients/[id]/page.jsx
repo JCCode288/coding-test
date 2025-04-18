a35a3e67b62c99e49cd7953f60c5c32ccdf349da
jc288 <jendycaprico@gmail.com>
2025-04-18 16:51:11 +0700

    fix(frontend): missing page header and sidebar open button

--- frontend/src/app/clients/[id]/page.jsx
+++ frontend/src/app/clients/[id]/page.jsx
@@ -1,4 +1,5 @@
 import ClientsDetail from "@/components/clients-detail";
+import PageHeader from "@/components/page-header";
 import { getClientsById } from "@/lib/api";
 
 export default async function ClientDetail({ params }) {
@@ -8,8 +9,11 @@ export default async function ClientDetail({ params }) {
    const { data: client } = await getClientsById(id);
 
    return (
-      <section className="flex-1 m-8">
-         <ClientsDetail client={client} />
-      </section>
+      <>
+         <PageHeader name="Client Detail" />
+         <section className="flex-1 m-8">
+            <ClientsDetail client={client} />
+         </section>
+      </>
    );
 }
